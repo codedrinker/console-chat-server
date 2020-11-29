2f764b1f0bbeb83c2836bef06cb6c61f8ddf6b19
醉鬼 <gwangchunlei@gmail.com>
2020-11-29 16:55:28 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ mvn clean compile -DskipTests package
 pid=$(ps aux | grep "console-chat-server" | grep -v grep | awk '{print $2}')
 echo "kill -9 $pid"
 kill -9 $pid
-nohup java -jar target/console-chat-server-1.0.0.jar >/dev/null 2>&1 &
+nohup java -jar target/console-chat-server-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
